c781d3c529888de2125b61f37833633f94906a85
Alice Rey <Alice Rey>
2016-12-14 10:54:27 +0100

    meine Aenderungen

--- NumProgAufg2/src/numprogaufg2/Testi.java
+++ NumProgAufg2/src/numprogaufg2/Testi.java
@@ -1,14 +1,14 @@
 
 public class Testi {
     public static void main(String[] args){
-        double C[][] = { {0,4,8,3},{0,2,2,4},{0,-3,-7,2},{0,1,5,2} };
-        double b[] = { 7,0,1,2};
-       printvektor(b);
+        double C[][] = { {-0.1, 0.05, 0.333333},{0.05, -0.525, 0.333},{0.05, 0.475, -0.666666667} };
+       // double b[] = { -6,-17,-23,2};
+       //printvektor(b);
        //double x[] = Gauss.solve(C, b);
        double x[] = Gauss.solveSing(C);
        printvektor(x);
-       x = Gauss.solve(C, b);
-       printvektor(x);
+       //x = Gauss.solve(C, b);
+       //printvektor(x);
     }
     
     private static void printvektor(double b[]){
